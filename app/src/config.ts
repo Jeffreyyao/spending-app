f12f2720c161b1a1176a53fffc4c3e4b9d936e12
jeffreyyao <jeffreyyao@outlook.com>
2025-08-16 11:05:03 +0800

    fix: change to production api url

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    // API_BASE_URL: "https://accounting-api.zyaoaq.workers.dev",
-    API_BASE_URL: "http://localhost:8787",
+    API_BASE_URL: "https://accounting-api.zyaoaq.workers.dev",
+    // API_BASE_URL: "http://localhost:8787",
     DB: "accounting-0",
 }
